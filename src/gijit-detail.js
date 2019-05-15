c50798d3f8de8c705f2b8de7d79c5fbe37f982e6
Ganesh Rao <ganeshnrao@me.com>
2019-05-15 13:12:50 -0700

    Fix issue with detail view

--- src/gijit-detail.js
+++ src/gijit-detail.js
@@ -1,12 +1,12 @@
 const _ = require("lodash");
 const program = require("commander");
 const getApi = require("./getApi");
-const { execute, getKeyArg } = require("./utils");
+const { execute, getIssueKeyFromArgOrBranch } = require("./utils");
 const { fields, getDetailTable } = require("./renderer");
 
 async function main() {
   const [key] = program.parse(process.argv).args;
-  const issueKey = await getKeyArg(key);
+  const issueKey = await getIssueKeyFromArgOrBranch(key);
   const api = await getApi();
   const query = `key=${issueKey}`;
   const { issues } = await api.searchJira(query, { fields: _.keys(fields) });
